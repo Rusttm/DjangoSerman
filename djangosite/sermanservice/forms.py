28c4ad6bec321e706abe999c79fcc5aa9016a8fd
Rustam Mazhatov <rusttm@vm2205279990.vds.ru>
2022-06-30 17:36:55 +0300

    on vds

--- djangosite/sermanservice/forms.py
+++ djangosite/sermanservice/forms.py
@@ -16,16 +16,16 @@ class ReportForm(Form):
     to_date = DateField(label="To Date", widget=widgets.DateInput(attrs={'type': 'date'}))
     type_holder = forms.ChoiceField(choices=holder_types)
 
-class ReportForm2(ModelForm):
 
-    report_types = ( ('AccountSum', 'AccountSum'),
-    ('Sales taxbook', 'Sales taxbook'),
-    ('Purchases taxbook', 'Purchases taxbook'),
-    ('Profit report', 'Profit report'),
-    ('Agents report', 'Agents report'),
-    )
-    holder_types = (('Main', 'Main'),('Nsk', 'Nsk'),('Pfo', 'Pfo'),)
+class ParsingForm(Form):
+
+    report_types = ( ('Choose site for parsing', 'Choose site for parsing'),
+                     ('All sites parsing', 'All sites parsing'),
+                     ('Serman site', 'Serman site'),
+                     ('Forest site', 'Forest site'),
+                     ('Pakt site', 'Pakt site'),
+                     ('Cascad site', 'Cascad site'),
+                     )
+
     type_report = forms.ChoiceField(choices=report_types)
-    from_date = DateField(label="From Date", widget=widgets.DateInput(attrs={'type': 'date'}))
-    to_date = DateField(label="To Date", widget=widgets.DateInput(attrs={'type': 'date'}))
-    type_holder = forms.ChoiceField(choices=holder_types)
+
