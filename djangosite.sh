6351e75ef12737e6f929a13c918c492e1959b5b5
rusttm <rustammazhatov@gmail.com>
2023-10-30 15:03:38 +0300

    initial msi commit

---
+++ djangosite.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# cd /home/rusttm/PycharmProjects/DjangoService/DjangoSerman/djangosite
+/home/rusttm/PycharmProjects/DjangoService/bot_env/bin/python manage.py runserver 185.132.199.5:8001 >> /home/rusttm/PycharmProjects/DjangoService/DjangoSerman/myapp.log 2>&1
